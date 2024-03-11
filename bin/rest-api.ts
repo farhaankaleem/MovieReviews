9c5f47ed9baf98390c023a9a94f4e83e9674db34
Farhaan Kaleem <farhaan.kaleem@gosite.com>
2024-03-11 01:02:00 +0000

    Adding Authentication Authorisation

--- bin/rest-api.ts
+++ bin/rest-api.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
-import { RestAPIStack } from "../lib/rest-api-stack";
+import { AuthAppStack } from "../lib/auth-app-stack";
 
 const app = new cdk.App();
-new RestAPIStack(app, "RestAPIStack", { env: { region: "us-east-1" } });
+new AuthAppStack(app, "AuthAppStack", { env: { region: "us-east-1" } });
--- shared/types.d.ts
+++ shared/types.d.ts
@@ -17,3 +17,19 @@ export type MovieReviewQueryParams = {
 export type MovieReviewEdit = {
   content?: string;
 }
+
+export type SignUpBody = {
+  username: string;
+  password: string;
+  email: string
+}
+
+export type ConfirmSignUpBody = {
+  username: string;
+  code: string;
+}
+
+export type SignInBody = {
+  username: string;
+  password: string;
+}
