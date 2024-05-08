766d8007a94b5d96c8b6973df2efc8e27e99fefb
farhaankaleem <farhaan.kaleem@gmail.com>
2024-05-09 00:18:30 +0100

    S3 Bucket Deploy

--- lib/auth-app-stack.ts
+++ lib/auth-app-stack.ts
@@ -5,6 +5,9 @@ import { AuthApi } from './auth-api'
 import {AppApi } from './app-api'
 import * as path from 'path';
 import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as s3deploy from "aws-cdk-lib/aws-s3-deployment";
+
 export class AuthAppStack extends cdk.Stack {
 
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -29,6 +32,24 @@ export class AuthAppStack extends cdk.Stack {
       compatibleRuntimes: [lambda.Runtime.NODEJS_16_X],
       description: "It has types-schema.json file, which is used by almost all the files for validation."
     });
+
+    const siteBucket = new s3.Bucket(this, "SiteBucket", {
+      publicReadAccess: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ACLS,
+      accessControl: s3.BucketAccessControl.BUCKET_OWNER_FULL_CONTROL,
+      websiteIndexDocument: "index.html"
+    });
+
+    new s3deploy.BucketDeployment(this, "DeployWebsite", {
+      sources: [s3deploy.Source.asset("./dist")],
+      destinationBucket: siteBucket
+    });
+
+    new cdk.CfnOutput(this, "WebsiteURL", {
+      value: siteBucket.bucketWebsiteUrl
+    })
 
     new AuthApi(this, 'AuthServiceApi', {
       userPoolId: userPoolId,
