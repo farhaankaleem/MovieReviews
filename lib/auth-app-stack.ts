a2ccc9e646d96354d22ed6195c593e1bd22703c5
farhaankaleem <farhaan.kaleem@gmail.com>
2024-05-09 15:20:25 +0100

    Frontend CDN deployment to AWS

--- lib/auth-app-stack.ts
+++ lib/auth-app-stack.ts
@@ -1,68 +1,84 @@
-import * as cdk from "aws-cdk-lib";
-import { Construct } from "constructs";
-import { UserPool } from "aws-cdk-lib/aws-cognito";
+import { CfnOutput, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
+import { BlockPublicAccess, Bucket, BucketAccessControl } from 'aws-cdk-lib/aws-s3';
+import { Construct } from 'constructs';
+import { CloudFrontWebDistribution } from 'aws-cdk-lib/aws-cloudfront';
+import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
 import { AuthApi } from './auth-api'
 import {AppApi } from './app-api'
 import * as path from 'path';
 import * as lambda from "aws-cdk-lib/aws-lambda";
-import * as s3 from "aws-cdk-lib/aws-s3";
-import * as s3deploy from "aws-cdk-lib/aws-s3-deployment";
+import * as cognito from 'aws-cdk-lib/aws-cognito';
 
-export class AuthAppStack extends cdk.Stack {
-
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class AuthAppStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const userPool = new UserPool(this, "UserPool", {
+    const userPool = new cognito.UserPool(this, "UserPool", {
       signInAliases: { username: true, email: true },
       selfSignUpEnabled: true,
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      removalPolicy: RemovalPolicy.DESTROY,
     });
-
     const userPoolId = userPool.userPoolId;
-
     const appClient = userPool.addClient("AppClient", {
       authFlows: { userPassword: true },
     });
-
     const userPoolClientId = appClient.userPoolClientId;
-
     const codeLayer = new lambda.LayerVersion(this, 'codeLayer', {
       code: lambda.Code.fromAsset(path.resolve(__dirname, __dirname+'/../../code-layer/')),
       compatibleRuntimes: [lambda.Runtime.NODEJS_16_X],
       description: "It has types-schema.json file, which is used by almost all the files for validation."
     });
 
-    const siteBucket = new s3.Bucket(this, "SiteBucket", {
-      publicReadAccess: true,
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
-      autoDeleteObjects: true,
-      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ACLS,
-      accessControl: s3.BucketAccessControl.BUCKET_OWNER_FULL_CONTROL,
-      websiteIndexDocument: "index.html"
-    });
-
-    new s3deploy.BucketDeployment(this, "DeployWebsite", {
-      sources: [s3deploy.Source.asset("./dist")],
-      destinationBucket: siteBucket
-    });
-
-    new cdk.CfnOutput(this, "WebsiteURL", {
-      value: siteBucket.bucketWebsiteUrl
-    })
-
     new AuthApi(this, 'AuthServiceApi', {
       userPoolId: userPoolId,
       userPoolClientId: userPoolClientId,
       codeLayer: codeLayer
     });
-
     new AppApi(this, 'AppApi', {
       userPoolId: userPoolId,
       userPoolClientId: userPoolClientId,
       codeLayer: codeLayer
     } );
 
-  } 
+    const siteBucket = new Bucket(this, "SiteBucket", {
+      removalPolicy: RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      blockPublicAccess: BlockPublicAccess.BLOCK_ACLS,
+      accessControl: BucketAccessControl.BUCKET_OWNER_FULL_CONTROL,
+      websiteIndexDocument: "index.html",
+    });
 
+    new BucketDeployment(this, "DeployWebsite", {
+      sources: [Source.asset("./dist")],
+      destinationBucket: siteBucket,
+    });
+
+    const oai = new cloudfront.OriginAccessIdentity(
+      this,
+      "OriginAccessIdentity"
+    );
+    siteBucket.grantRead(oai);
+
+    const distribution = new CloudFrontWebDistribution(
+      this,
+      "ReactDeploymentDistribution",
+      {
+        originConfigs: [
+          {
+            s3OriginSource: {
+              s3BucketSource: siteBucket,
+              originAccessIdentity: oai,
+            },
+            behaviors: [{ isDefaultBehavior: true }],
+          },
+        ],
+      }
+    );
+
+    new CfnOutput(this, "CloudFrontDistributionDomainName", {
+      value: distribution.distributionDomainName,
+      description: "CloudFront Distribution Domain Name",
+    });
+  }
 }
